9a96b79ebe3de7e2f2cc4c61d86c18f17ee247c7
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2022-10-06 16:39:11 +0700

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,27 @@
+
+#y= [98, 35, 42, 21, 29, 309, 46]
+#print(y)
+#summ = 0
+#for i in range(1, len(y), 2):
+#    if i % 2 == 1:
+#        summ += y[i]       
+#print(f"{y} -> Cумма элементов на нечётных позициях: {summ}")
+
+#Задача 2
+#def mult_lst(lst):
+#    l = len(lst)//2 + 1 if len(lst) % 2 != 0 else len(lst)//2
+#    = = [lst[i]*lst[len(lst)-i-1] for i in range (l)]
+#    print(new_lst)
+
+#= = [2, 3, 4, 5, 6]
+#mult_lst(lst)
+#lst = list(map(int, input("Введите числа через пробел:\n").split()))
+#mult_lst(lst)
+
+#Задача 3
+#n = int(input('Введите значение числа "а": ')) 
+#a = ''
+#while n > 0:
+#    a = str(n % 2) + a
+#    n = n // 2
+#print(f'число "а" в двоичной системе: {a}')
