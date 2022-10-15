a55515ce74c54a5615f3d7ade387ddbdb29094c1
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2022-10-15 12:03:00 +0700

    Add files via upload

---
+++ HW5.py
@@ -0,0 +1,49 @@
+#Задача 1:
+
+#my_text = 'Напишите абв напиабв програбвмму программу, удаляющую из \
+# этого абв текста все вабвс слова, содерабващие содержащие "абв"'
+
+#def del_some_words (my_text):
+#    my_text = list(filter(lambda x: 'абв'  notin x, my_text.split()))
+#    " " " ".join(my_text)
+
+#del_some_words = my_text(my_text)
+#print(my_text)
+
+
+#Задача 2:
+
+#with open('text_for_RLE_024.txt', 'w', encoding='UTF-8') as file:
+#    file.write(input('Напишите текст необходимый для сжатия: '))
+#with open('text_for_RLE_024.txt', 'r') as file:
+#    my_text = file.readline()
+#    text_compression = my_text.split()
+
+#print(my_text)
+
+
+#def rle_encode (text):
+#    enconding  = "
+#    prev_char = "
+#    count  =  1
+#    if not text:
+#        return "
+
+#    for char in text:
+#        if != != prev_char:
+#            if prev_char:
+#                enconding += str (count) + prev_char
+#            count  =  1
+#            prev_char = char
+#        else:
+#            count  += 1
+#    else:
+#        enconding += str(count) + prev_char
+#        return enconding
+
+
+#text_compression = rle_encode(my_text)
+
+#with open('text_compression_RLE_024.txt', 'w', encoding='UTF-8') as file:
+#    file.write(f'{text_compression}')
+#print(text_compression)
